0ba8f98f35e4ac66f61403e18015d5f92c6a2c02
degero <chadpaynter@hotmail.com>
2019-05-14 16:13:49 +0700

    initial setup

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+var Home = React.createClass({
+   render: function() {
+       return (
+            <div className="jumbotron">
+            <h1>Hello world</h1>
+            <p>asdfasdf</p>
+            </div>
+       );
+   }
+});
+
+module.exports = Home;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var Home = require('./components/homePage');
+
+
+React.render(<Home />, getelementById('app'));
